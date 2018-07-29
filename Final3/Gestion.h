0ae1f048a634bbd5a97bec9d46e56e1a16d6cdac
elyfalcon <elyfalcon@yahoo.com>
2018-07-29 19:52:03 -0300

    final3

---
+++ Final3/Gestion.h
@@ -0,0 +1,9 @@
+#ifndef GESTION_H_INCLUDED
+#define GESTION_H_INCLUDED
+
+ArrayList* Lista_Localidad(ArrayList* this);
+
+ArrayList* al_eliminaDuplicados(ArrayList* this, int (*pFunc)(void* ,void*));
+ArrayList* Gestion_Localidades(ArrayList* this);
+
+#endif // GESTION_H_INCLUDED
